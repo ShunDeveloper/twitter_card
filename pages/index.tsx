4530a676909b939c32824cc4097505d9ad3f8432
Shunsuke Tsuchiya <s.tsuchiya.business@gmail.com>
2022-09-08 17:30:14 +0900

    update meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,9 +23,19 @@ const Home: NextPage = () => {
       <Head>
         <title>How to use Twitter Card</title>
         <meta name="description" content="How to use Twitter Card" />
+        <meta property="og:type" content="summary_large_image" />
         <meta
           property="og:image"
           content="https://twitter-card-nu.vercel.app/twitter_card.png"
+        />
+        <meta property="og:title" content="How to use Twitter Card" />
+        <meta
+          property="og:description"
+          content="Twitter Cardを自分のブログに導入してみたいと思い練習で作成しました. 下記に導入のやり方を書いたので良ければご参照下さい"
+        />
+        <meta
+          property="og:image:alt"
+          content="私のブログで使う予定のTwitterカード"
         />
         <link rel="icon" href="/favicon.ico" />
         <meta name="twitter:card" content="summary_large_image" />
